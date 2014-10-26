5e758fa86ed8402991755de13b81bbf417784ff0
Nilo Redini <n.redini@gmail.com>
2014-10-26 16:17:02 -0700

    added dropdown

--- app/models/dinner.rb
+++ app/models/dinner.rb
@@ -1,4 +1,5 @@
 class Dinner < ActiveRecord::Base
+  CATEGORIES = ["American", "Chinese", "Italian", "Japanese", "Indian"]
 	validates :location, :description, :title, :category, :price, :seats_available, presence: true
 	validates_numericality_of :seats_available, :price, :greater_than_or_equal_to =>0
 	validate :not_past_date
