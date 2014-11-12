74e67834a003277c1a117e9f9ce38151eb8198ea
Nilo Redini <n.redini@gmail.com>
2014-11-12 14:32:24 -0800

    added first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,21 @@ Rails.application.routes.draw do
   get 'home/login'
   get 'home/about'
   get 'home/contact'
+
   authenticated :user do
     root :to => "dinners#index", :as => :auth_root
     # Rails 4 users must specify the 'as' option to give it a unique name
     # root :to => "main#dashboard", :as => "authenticated_root"
   end  
   root 'home#welcome'
+
+  
   get 'dinners/join/:id' => 'dinners#join', as: :join_dinner
   get 'dinners/leave/:id' => 'dinners#leave', as: :leave_dinner
 
   resources :comments
   resources :dinners
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   
 
