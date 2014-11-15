678b5e402bf3cff06e695bba8211d715c30fb83a
Nilo Redini <n.redini@gmail.com>
2014-11-14 18:52:51 -0800

    added default picture in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ user1.last_name = 'Redini'
 user1.email = 'lol@lol.it'
 user1.password = 'asdasdasd'
 user1.password_confirmation = 'asdasdasd'
-user1.image_file_name = "johndoe.jpg"
+user1.image = File.open(Rails.root.join('app', 'assets', 'images', 'medium', 'johndoe.jpg'))
 user1.save!
 
 75.times do 
