7102abf2bc1c06b118e8a5779d8158eac6fd8786
Nilo Redini <n.redini@gmail.com>
2014-11-15 01:43:17 -0800

    actual price of a suppr and facebook picture

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ gem 'mysql2', group: :production
 
 
 
+gem 'open_uri_redirections'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,26 @@
    has_many :reservations
    has_many :dinners, :through => :reservations
    has_many :comments
-   has_attached_file :image, 
-   :styles => { 
-     :medium => "300x300>", 
-     :small => "200x200>", 
-     :thumb => "100x100>" 
+   has_attached_file :image,
+   :styles => {
+     :medium => "300x300>",
+     :small => "200x200>",
+     :thumb => "100x100>"
    },
    :default_url => ":style/johndoe.jpg"
-   
+
    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/jfif"]
-   
+
    devise :database_authenticatable, :registerable,
    :confirmable, #Add this for email confirmation
    :recoverable, :rememberable, :trackable, :validatable,
    :omniauthable, :omniauth_providers => [:facebook]
-   
+
    validates :first_name, :email, # :last_name,
    presence: true
 
-	validates_numericality_of :n_joined, :n_hosted, :greater_than_or_equal_to =>0
-   
+  validates_numericality_of :n_joined, :n_hosted, :greater_than_or_equal_to =>0
+
    def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
        user.email = auth.info.email
@@ -31,10 +31,13 @@
        # user.image = auth.info.image
        # DB workaround
        user.last_name = ' '
-       # user.image = auth.info.image # assuming the user model has an image
+       if auth.info.image.present?
+         avatar_url = process_uri(auth.info['image']['large'])
+         user.update_attribute(:image, URI.parse(avatar_url))
+       end
      end
    end
-   
+
    def self.new_with_session(params, session)
      super.tap do |user|
        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
@@ -42,7 +45,16 @@
        end
      end
    end
-   
+
+   private
+   def self.process_uri(uri)
+     require 'open-uri'
+     require 'open_uri_redirections'
+     open(uri, :allow_redirections => :safe) do |r|
+       r.base_uri.to_s
+     end
+   end
+
  end
 
 def only_if_unconfirmed
