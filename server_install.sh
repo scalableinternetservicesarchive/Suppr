18cd76c8b40ce6c41307bb2a26849c288ff8e2b5
greg <gp_github@swimr.com>
2014-11-15 17:34:27 -0800

    Added deployment scripts

---
+++ server_install.sh
@@ -0,0 +1,7 @@
+#Run this on the ec2 server from the application root directory
+#/var/www/Suppr/production/current/
+#/var/www/Suppr/staging/current/
+bundle install --without development test;
+rake assets:precompile RAILS_ENV=production
+sudo service httpd restart
+sudo chmod 0700 install.sh
