b8f55320cbe8b5fcbf4cc4533778f70c571f06ec
greg <gp_github@swimr.com>
2014-11-15 23:31:21 -0800

    Small addition to install script

--- runOnServerOnly.sh
+++ runOnServerOnly.sh
@@ -1,6 +1,10 @@
 #Run this on the ec2 server from the application root directory
 #/var/www/Suppr/production/current/
 #/var/www/Suppr/staging/current/
+
+#Set  envirnment variables
+source /var/www/SupprSetup/*
+
 bundle install --without development test;
 rake assets:precompile RAILS_ENV=production
 sudo service httpd restart
