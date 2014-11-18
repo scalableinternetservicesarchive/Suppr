78e7de201784ab2e4e28b6da0c93856523246cea
Nilo Redini <n.redini@gmail.com>
2014-11-17 20:54:57 -0800

    fixed bug in user pages and changed css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   def show
   	@user = User.find(params[:id])
-    @reservations = Reservation.find_by(:user => @user)
+    @reservations = Reservation.where(:user => @user.id)
+    puts "HEREEEEEEEEEE"
+    puts @reservations.length
   end
 end
