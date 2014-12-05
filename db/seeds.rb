fb62b13e083fd1fe7ff8effbf31cec80fe1fda2a
greg <gp_github@swimr.com>
2014-12-05 13:27:18 -0800

    change to standalone mysql server

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-N = 10000
 
 user1 = User.new
 user1.first_name = 'Nilo'
