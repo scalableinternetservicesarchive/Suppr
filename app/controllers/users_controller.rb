936050e1645e6a956a2b25b55e5a313aba8eb567
Nilo Redini <n.redini@gmail.com>
2014-12-11 23:36:00 -0800

    caching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  # before_action :authenticate_user!, only: [:show]
   def show
     @user = User.find(params[:id])
-    @reservations = Reservation.where(:user => @user.id)
+    @reservations = []
+    if @user.n_joined > 0
+      @reservations = Reservation.where(:user => @user.id)
+    end
   end
 end
