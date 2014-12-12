6b0586d543026aaf540efce886e9ce4ae31c7ee1
Nilo Redini <n.redini@gmail.com>
2014-12-11 21:37:02 -0800

    cache for kaminari

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -6,7 +6,7 @@ class DinnersController < ApplicationController
   # GET /dinners.json
 
   def index
-    @dinners = Dinner.order('date').page(params[:page]).per(25) # (:order => 'dinner.date DESC')
+    @dinners = Dinner.fetch_page(:page => (params[:page] || 1), :per => 10, :order => :date) # (:order => 'dinner.date DESC')
   end
 
   # GET /dinners/1
